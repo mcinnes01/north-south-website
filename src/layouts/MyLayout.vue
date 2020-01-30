44c83669d5b906cbf821e00d4f78c27d7fbc26f9
iamandymcinnes <andy.mcinnes@kalibrate.com>
2020-01-30 10:07:41 +0000

    first commit

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img height="50px" src="~assets/northsouth.svg">
+          </q-avatar>
+          North South Brewing
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<script>
+export default {
+  name: 'MyLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
